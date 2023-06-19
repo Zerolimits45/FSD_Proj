2d1bb55a5aa06e74dd348ed95611db86280c45dd
Aloysius <zerolimits45@gmail.com>
2023-06-19 22:52:47 +0800

    this took too long

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,12 +2,15 @@ import { useState } from 'react'
 
 import './App.css'
 import Home from './Pages/Home'
+import MenuIndicator from './Components/Navbar'
+import Navbar from './Components/Navbar'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+      <Navbar />
       <Home />
     </>
   )
