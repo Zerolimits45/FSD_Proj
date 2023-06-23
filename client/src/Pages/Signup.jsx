b4f608eb081766e2d42f8dfd3de044280c6928d9
TanBenneT <tanbennet02@gmail.com>
2023-06-23 18:00:15 +0800

    Sign Up Done

--- client/src/Pages/Signup.jsx
+++ client/src/Pages/Signup.jsx
@@ -1,13 +1,17 @@
-import { Avatar, Grid, Paper, Typography, TextField, Button } from '@mui/material'
-import React, {useState} from 'react'
+import { Avatar, Grid, Paper, Typography, TextField, Button, Box } from '@mui/material'
+import React, { useState } from 'react'
 import AddCircleIcon from '@mui/icons-material/AddCircle';
 import { useFormik } from 'formik'
 import * as Yup from 'yup'
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom';
+import http from '../http.js';
+
 function Signup() {
     const paperStyle = { padding: 20, width: 400, margin: '20px auto' }
     const fieldspacing = { margin: '10px 0' }
     const [isSubmitting, setIsSubmitting] = useState(false)
+    const navigate = useNavigate();
+    const regEx = /^[89]{1}\d{7}$/
     const formik = useFormik({
         initialValues: {
             name: '',
@@ -17,16 +21,33 @@ function Signup() {
             confirmpassword: ''
         },
         validationSchema: Yup.object({
-            name: Yup.string().min(3, 'Minimum 3 characters').required('Required'),
-            email: Yup.string().email('Invalid email format').required('Required'),
-            phone: Yup.number().typeError('Enter a valid phone number').required('Required'),
-            password: Yup.string().min(8, 'Minimum 8 characters').required('Required'),
+            name: Yup.string().trim().min(5, 'Minimum 5 characters').required('Required'),
+            email: Yup.string().trim().email('Invalid email format').required('Required'),
+            phone: Yup.string().trim().min(8).max(8).matches(regEx, "Phone is Invalid").required('Required'),
+            password: Yup.string().trim().min(8, 'Minimum 8 characters').required('Required'),
             confirmpassword: Yup.string().oneOf([Yup.ref('password'), null], 'Passwords must match').required('Required')
         }),
-        onSubmit: values => {
-            setIsSubmitting(true)
-            console.log(values)
-        }
+        onSubmit: (data) => {
+            data.name = data.name.trim();
+            data.email = data.email.trim();
+            data.phone = data.phone.trim();
+            data.password = data.password.trim();
+            http.post('/user/register', data)
+                .then((res) => {
+                    console.log(res.data)
+                    navigate("/login")
+                })
+                .catch((error) => {
+                    if (error.response && error.response.status === 400) {
+                        const errorMessage = error.response.data.message;
+                        formik.setErrors({
+                            ...formik.errors,
+                            email: errorMessage,
+                        });
+                    }
+                })
+        },
+
     })
     return (
         <Grid>
@@ -38,7 +59,7 @@ function Signup() {
                     </Typography>
                     <Typography variant='caption' gutterBottom>Please fill up the form to create an account</Typography>
                 </Grid>
-                <form onSubmit={formik.handleSubmit}>
+                <Box component="form" onSubmit={formik.handleSubmit}>
                     <TextField
                         name="name"
                         label='Name'
@@ -66,7 +87,6 @@ function Signup() {
                         name="phone"
                         label='Phone'
                         fullWidth
-                        type='number'
                         onChange={formik.handleChange}
                         value={formik.values.phone}
                         error={formik.touched.phone && Boolean(formik.errors.phone)}
@@ -97,8 +117,8 @@ function Signup() {
                         style={fieldspacing}
                         placeholder='Confirm your password'
                     />
-                    <Button type="submit" color='primary' variant='contained' disabled={isSubmitting} fullWidth style={fieldspacing}>{isSubmitting ? "Loading" : "Sign Up"}</Button>
-                </form>
+                    <Button type="submit" color='primary' variant='contained' fullWidth style={fieldspacing}>Sign Up</Button>
+                </Box>
                 <Typography> Already have an account?
                     <Link to={'/Login'}>
                         Log in
