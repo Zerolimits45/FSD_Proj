e67b3cca11071638d23180b6ba9915904d1aecda
Aloysius <zerolimits45@gmail.com>
2023-07-26 22:00:11 +0800

    Server changes and removal

--- server/index.js
+++ server/index.js
@@ -1,12 +1,5 @@
 const express = require('express');
 const router = express.Router();
-const { validateToken } = require('../middlewares/auth');
-const { upload } = require('../middlewares/upload');
-router.post('/upload', validateToken, upload, (req, res) => {
-  res.json({ filename: req.file.filename });
-});
-module.exports = router;
-
 const cors = require('cors');
 const session = require('express-session')
 require('dotenv').config();
