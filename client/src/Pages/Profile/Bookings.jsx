5f133d716717a41f51ca9551a36bd8955f803fa9
TanBenneT <tanbennet02@gmail.com>
2023-07-27 15:59:06 +0800

    bookings done left edit and delete on admin side

--- client/src/Pages/Profile/Bookings.jsx
+++ client/src/Pages/Profile/Bookings.jsx
@@ -1,60 +1,81 @@
-import React from 'react'
+import React, { useState, useEffect, useContext } from 'react'
 import { Typography, Grid, Container, TextField, Box, Button, Card, CardContent, Divider } from '@mui/material'
 import { Link } from 'react-router-dom'
 import AccessTimeFilledIcon from '@mui/icons-material/AccessTimeFilled';
+import http from '../../http';
+import UserContext from '../../contexts/UserContext.js';
+import { differenceInDays } from 'date-fns';
 
 function Bookings() {
   const completestyle = { backgroundColor: '#228B22', margin: '8px 0', fontWeight: 'bold', color: 'white' }
   const btnstyle = { margin: '8px 0', fontWeight: 'bold', color: 'white', marginLeft: '10px' }
   const dividerstyle = { backgroundColor: '#150039', fontWeight: 'bold', margin: '10px 0' }
-  const textstyle = { color: '#150039'}
+  const textstyle = { color: '#150039' }
+
+  const [bookingList, setBookingList] = useState([]);
+  const { user } = useContext(UserContext)
+
+  useEffect(() => {
+    http.get(`/booking/user/${user.id}`).then((res) => {
+      console.log(res.data)
+      setBookingList(res.data);
+    })
+  }, [])
+
   return (
     <Container maxWidth='xl'>
       <Typography variant='h6' color="white" marginBottom={2}>
         Your Bookings
       </Typography>
-      <Grid container>
-        <Grid item xs={12} md={12}>
-          <Card elevation={5}>
-            <CardContent>
-              <Grid container spacing={2}>
-                <Grid item xs={12} md={8}>
-                  <Box>
-                    <img src="../images/VW.png" alt="" width={'80%'} height={'300px'} style={{ objectFit: 'contain' }} />
-                  </Box>
-                </Grid>
-                <Grid item xs={12} md={4} display={'flex'} alignItems={'center'}>
-                  <Box style={{ display: 'flex', flexDirection: 'column' }}>
-                    <Typography variant='h5' fontWeight={600} style={textstyle}>
-                      Booking Details:
-                    </Typography>
-                    <Typography variant='h5' fontWeight={600} style={textstyle}>
-                    stuff | stuff
-                    </Typography>
-                    <Typography variant='h6' fontWeight={600} style={textstyle}>
-                      <AccessTimeFilledIcon fontSize='small' /> stuff days
-                    </Typography>
-                    <Typography variant='h6' fontWeight={600} style={textstyle}>
-                      Total: $ price
-                    </Typography>
-                    <Divider style={dividerstyle} />
-                    <Typography variant='h6' fontWeight={600} style={textstyle}>
-                      Status: status
-                    </Typography>
-                  </Box>
-                </Grid>
-                <Grid item xs={12} md={12} display={'flex'} justifyContent={'center'}>
-                  <Button variant='contained' style={completestyle}>Rate Experience</Button> 
-                  <Button color='btn' variant='contained' style={btnstyle}>Cancel Booking</Button>
-                </Grid>
-              </Grid>
-            </CardContent>
-          </Card>
-        </Grid>
-      </Grid>
+      {
+        bookingList.map((booking) => (
+          <Grid container>
+            <Grid item xs={12} md={12}>
+              <Card elevation={5}>
+                <CardContent>
+                  <Grid container spacing={2}>
+                    <Grid item xs={12} md={8}>
+                      <Box>
+                        <img src="../images/VW.png" alt="" width={'80%'} height={'300px'} style={{ objectFit: 'contain' }} />
+                      </Box>
+                    </Grid>
+                    <Grid item xs={12} md={4} display={'flex'} alignItems={'center'}>
+                      <Box style={{ display: 'flex', flexDirection: 'column' }}>
+                        <Typography variant='h5' fontWeight={600} style={textstyle}>
+                          Booking Details:
+                        </Typography>
+                        <Typography variant='h5' fontWeight={600} style={textstyle}>
+                          {booking.car.model} {booking.car.make} | {booking.car.type}
+                        </Typography>
+                        <Typography variant='h6' fontWeight={600} style={textstyle}>
+                          <AccessTimeFilledIcon fontSize='small' /> {differenceInDays(new Date(booking.enddate), new Date(booking.startdate))} days
+                        </Typography>
+                        <Typography variant='h6' fontWeight={600} style={textstyle}>
+                          Total: $ {booking.price}
+                        </Typography>
+                        <Divider style={dividerstyle} />
+                        <Typography variant='h6' fontWeight={600} style={textstyle}>
+                          Status: {booking.status}
+                        </Typography>
+                      </Box>
+                    </Grid>
+                    <Grid item xs={12} md={12} display={'flex'} justifyContent={'center'}>
+                      {booking.status == 'Completed' &&
+                        <Button variant='contained' style={completestyle} LinkComponent={Link} to={'/profile/bookings/rating'}>
+                          Rate Experience
+                        </Button>
+                      }
+                      <Button color='btn' variant='contained' style={btnstyle}>Cancel Booking</Button>
+                    </Grid>
+                  </Grid>
+                </CardContent>
+              </Card>
+            </Grid>
+          </Grid>
+        ))
+      }
     </Container>
   )
 }
 
 export default Bookings
-
--- client/src/Pages/Profile/Rating_Booking.jsx
+++ client/src/Pages/Profile/Rating_Booking.jsx
@@ -45,35 +45,42 @@ function Rating_Booking() {
 
     return (
         <Container maxWidth='xl'>
-            <Typography variant='h5' color="white" marginTop={10} marginBottom={2} align='center'>
+            <Typography variant='h5' color="white" marginBottom={2} align='center'>
                 Thank you for riding with us!
             </Typography>
             {/* Rectangle Booking */}
             <Typography variant='h5' color="white" marginTop={10} marginBottom={2} align='center'>
                 We would appreciate if you could leave us a review!
             </Typography>
-            <Rating
-                name="rating"
-                size="large"
-                value={3}
-                onChange={(event, newValue) => {
-                    setValue(newValue);
-                }}
-            />
-            {/* cant center. also anyway to make larger? */}
-            <Typography variant='h5' color="white" marginTop={10} marginBottom={2} align='center'>
+            <Box display="flex" justifyContent="center" marginTop={5} >
+                {/* Centered star rating */}
+                <Rating
+                    name="rating"
+                    size="large"
+                    value={value}
+                    onChange={(event, newValue) => {
+                        setValue(newValue);
+                    }}
+                    sx={{
+                        fontSize: "4rem",
+                    }}
+                />
+            </Box>
+            <Typography variant='h5' color="white" marginTop={5} marginBottom={2} align='center'>
                 and do you have any feedback for us?
             </Typography>
-            <TextField
-                style={textfieldstyle}
-                id="outlined-multiline-static"
-                align='center'
-                multiline
-                rows={10}
-                placeholder="Leave us a Feedback!"
-                variant="outlined"
-            />
-            {/* also cant center */}
+            <Box display="flex" justifyContent="center">
+                {/* Centered text field */}
+                <TextField
+                    style={textfieldstyle}
+                    id="outlined-multiline-static"
+                    align='center'
+                    multiline
+                    rows={10}
+                    placeholder="Leave us a Feedback!"
+                    variant="outlined"
+                />
+            </Box>
         </Container>
     )
 }
