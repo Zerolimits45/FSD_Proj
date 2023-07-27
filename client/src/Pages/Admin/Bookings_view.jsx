79f225d007f776d7abf627755a2dfc039a83d0c0
Aloysius <zerolimits45@gmail.com>
2023-07-27 19:51:59 +0800

    Prevent complete from repeating

--- client/src/Pages/Admin/Bookings_view.jsx
+++ client/src/Pages/Admin/Bookings_view.jsx
@@ -4,30 +4,18 @@ import { DataGrid } from '@mui/x-data-grid';
 import http from '../../http'
 
 function RenderButton(props) {
-    const { hasFocus, value } = props;
-    const buttonElement = React.useRef(null);
-    const rippleRef = React.useRef(null);
+    const { booking } = props;
 
-    React.useLayoutEffect(() => {
-        if (hasFocus) {
-            const input = buttonElement.current?.querySelector('input');
-            input?.focus();
-        } else if (rippleRef.current) {
-            rippleRef.current.stop({});
-        }
-    }, [hasFocus]);
 
-    const [bookingList, setBookingList] = useState([]);
-    useEffect(() => {
-        http.get('/booking').then((res) => {
-            setBookingList(res.data);
-        })
-    }, [bookingList.map((booking) => booking.status)])
+    const handleCompleteBooking = () => {
+        http.put(`/booking/complete/${booking.id}`).then((res) => {
+            console.log(res.data);
+        });
+    }
 
     return (
         <>
             <Button
-                ref={buttonElement}
                 variant="contained"
                 size="small"
                 style={{ backgroundColor: '#6CA0DC' }}
@@ -35,28 +23,20 @@ function RenderButton(props) {
                 Edit
             </Button>
             <Button
-                ref={buttonElement}
                 variant="contained"
                 size="small"
                 style={{ marginLeft: 16, backgroundColor: '#C70000' }}
             >
                 Delete
             </Button>
-            {bookingList.map((booking) => (
-                <Button
-                    ref={buttonElement}
-                    variant="contained"
-                    size="small"
-                    style={{ marginLeft: 16, backgroundColor: '#228B22' }}
-                    onClick={() => {
-                        http.put(`/booking/complete/${booking.id}`).then((res) => {
-                            console.log(res.data)
-                        });
-                    }}
-                >
-                    Complete Booking
-                </Button>
-            ))}
+            <Button
+                variant="contained"
+                size="small"
+                style={{ marginLeft: 16, backgroundColor: '#228B22' }}
+                onClick={handleCompleteBooking}
+            >
+                Complete Booking
+            </Button>
         </>
 
 
@@ -71,7 +51,7 @@ function Bookings_view() {
         { field: 'enddate', headerName: 'End Date', width: 120 },
         { field: 'price', headerName: 'Price', width: 100 },
         { field: 'status', headerName: 'Status', width: 100 },
-        { field: 'action', headerName: 'Actions', width: 400, renderCell: RenderButton },
+        { field: 'action', headerName: 'Actions', width: 400, renderCell: (params) => <RenderButton booking={params.row} /> },
     ];
 
     const [bookingList, setBookingList] = useState([]);
