3c9927908e474e10bc397b21cdff8614c3e064d3
Aloysius <zerolimits45@gmail.com>
2023-07-27 19:14:42 +0800

    Bookings edit has issue

--- client/src/Pages/Admin/Bookings_Edit.jsx
+++ client/src/Pages/Admin/Bookings_Edit.jsx
@@ -7,7 +7,132 @@ import { useEffect, useState } from 'react'
 import http from '../../http'
 import { useSnackbar } from 'notistack';
 
-const btnstyle = { margin: '8px 0', fontWeight: 'bold', color: 'white' }
-const textfieldstyle = { backgroundColor: 'white', borderRadius: '5px', margin: '10px 0' }
-const textstyle = { color: '#150039', fontWeight: 'bold' }
+function Bookings_Edit() {
+    const btnstyle = { margin: '8px 0', fontWeight: 'bold', color: 'white' }
+    const textfieldstyle = { backgroundColor: 'white', borderRadius: '5px', margin: '10px 0' }
+    const textstyle = { color: '#150039', fontWeight: 'bold' }
+    
+    const [bookingList, setBookingList] = useState([]);
+    const { enqueueSnackbar } = useSnackbar();
 
+    const formik = useFormik({
+        initialValues: {
+            startDate: bookingList ? new Date(bookingList.startDate).toISOString().split('T')[0] : "",
+            endDate: bookingList ? new Date(bookingList.endDate).toISOString().split('T')[0] : "",
+            licencenumber: bookingList ? bookingList.licencenumber : "",
+            price: bookingList ? bookingList.price : "",
+        },
+        validationSchema: yup.object().shape({
+            startDate: yup.string().trim().required('Start date is required'),
+            endDate: yup.string().trim().required('End date is required'),
+            licencenumber: yup.string().trim().required('Licence number is required'),
+            price: yup.number().required('Price is required'),
+        }),
+        onSubmit: (data) => {
+            data.startDate = data.startDate.trim();
+            data.endDate = data.endDate.trim();
+            data.licencenumber = data.licencenumber.trim();
+            console.log(data);
+
+            http.put('/booking/' + id, data)
+                .then((res) => {
+                    enqueueSnackbar('Booking details saved', { variant: 'success' });
+                    console.log(res.data);
+                }
+                )
+        },
+        enableReinitialize: true
+    });
+  return (
+    <Container maxWidth='xl'>
+      <Box component="form" onSubmit={formik.handleSubmit}>
+        <Typography variant='h6' color="white" marginBottom={2}>
+          Edit time period of the car booking:
+        </Typography>
+        <Card>
+          <CardContent>
+            <Grid container spacing={2}>
+              <Grid item xs={12} md={6}>
+                <Typography variant='h7' style={textstyle}>
+                  Start Date:
+                </Typography>
+              </Grid>
+              <Grid item xs={12} md={6}>
+                <Typography variant='h7' style={textstyle}>
+                  End Date:
+                </Typography>
+              </Grid>
+
+              <Grid item xs={12} md={6}>
+                <TextField
+                  varient='filled'
+                  style={textfieldstyle}
+                  name='startDate'
+                  type='date'
+                  onChange={formik.handleChange}
+                  value={formik.values.startDate}
+                  error={formik.touched.startDate && Boolean(formik.errors.startDate)}
+                  helperText={formik.touched.startDate && formik.errors.startDate}
+                  placeholder='Booking start date'
+                  fullWidth
+                />
+              </Grid>
+              <Grid item xs={12} md={6}>
+                <TextField
+                  varient='filled'
+                  style={textfieldstyle}
+                  name='endDate'
+                  type='date'
+                  onChange={formik.handleChange}
+                  value={formik.values.endDate}
+                  error={formik.touched.endDate && Boolean(formik.errors.endDate)}
+                  helperText={formik.touched.endDate && formik.errors.endDate}
+                  placeholder='Booking end date'
+                  fullWidth
+                />
+              </Grid>
+            </Grid>
+          </CardContent>
+        </Card>
+
+        <Typography variant='h6' color="white" marginTop={10} marginBottom={2}>
+          Edit details of the booking:
+        </Typography>
+        <Card>
+          <CardContent>
+            <Grid container spacing={2}>
+              <Grid item xs={12} md={6}>
+                <TextField
+                  label="Licence number"
+                  varient='filled'
+                  style={textfieldstyle}
+                  name='licencenumber'
+                  onChange={formik.handleChange}
+                  value={formik.values.model}
+                  error={formik.touched.model && Boolean(formik.errors.model)}
+                  helperText={formik.touched.model && formik.errors.model}
+                  fullWidth
+                />
+              </Grid>
+              <Grid item xs={12} md={6}>
+                <TextField
+                  label="Price"
+                  varient='filled'
+                  style={textfieldstyle}
+                  name='price'
+                  onChange={formik.handleChange}
+                  value={formik.values.make}
+                  error={formik.touched.make && Boolean(formik.errors.make)}
+                  helperText={formik.touched.make && formik.errors.make}
+                  fullWidth
+                />
+              </Grid>
+            </Grid>
+          </CardContent>
+        </Card>
+        </Box>
+    </Container>
+  )
+}
+
+export default Bookings_Edit
--- client/src/Pages/Admin/Cars_Edit.jsx
+++ client/src/Pages/Admin/Cars_Edit.jsx
@@ -8,7 +8,7 @@ import http from '../../http'
 import { useSnackbar } from 'notistack';
 
 function Cars_Edit() {
-      //fetching car list
+  //fetching car list
   const [carList, setCarList] = useState(null);
   const { id } = useParams();
   const { enqueueSnackbar } = useSnackbar();
@@ -65,181 +65,181 @@ function Cars_Edit() {
   }, [])
   return (
     <Container maxWidth='xl'>
-    <Box component="form" onSubmit={formik.handleSubmit}>
-      <Typography variant='h6' color="white" marginBottom={2}>
-        Edit time period of your car registration:
-      </Typography>
-      <Card>
-        <CardContent>
-          <Grid container spacing={2}>
-            <Grid item xs={12} md={6}>
-              <Typography variant='h7' style={textstyle}>
-                Start Date:
-              </Typography>
-            </Grid>
-            <Grid item xs={12} md={6}>
-              <Typography variant='h7' style={textstyle}>
-                End Date:
-              </Typography>
-            </Grid>
+      <Box component="form" onSubmit={formik.handleSubmit}>
+        <Typography variant='h6' color="white" marginBottom={2}>
+          Edit time period of your car registration:
+        </Typography>
+        <Card>
+          <CardContent>
+            <Grid container spacing={2}>
+              <Grid item xs={12} md={6}>
+                <Typography variant='h7' style={textstyle}>
+                  Start Date:
+                </Typography>
+              </Grid>
+              <Grid item xs={12} md={6}>
+                <Typography variant='h7' style={textstyle}>
+                  End Date:
+                </Typography>
+              </Grid>
 
-            <Grid item xs={12} md={6}>
-              <TextField
-                varient='filled'
-                style={textfieldstyle}
-                name='startDate'
-                type='date'
-                onChange={formik.handleChange}
-                value={formik.values.startDate}
-                error={formik.touched.startDate && Boolean(formik.errors.startDate)}
-                helperText={formik.touched.startDate && formik.errors.startDate}
-                placeholder='Registration start date'
-                fullWidth
-              />
+              <Grid item xs={12} md={6}>
+                <TextField
+                  varient='filled'
+                  style={textfieldstyle}
+                  name='startDate'
+                  type='date'
+                  onChange={formik.handleChange}
+                  value={formik.values.startDate}
+                  error={formik.touched.startDate && Boolean(formik.errors.startDate)}
+                  helperText={formik.touched.startDate && formik.errors.startDate}
+                  placeholder='Registration start date'
+                  fullWidth
+                />
+              </Grid>
+              <Grid item xs={12} md={6}>
+                <TextField
+                  varient='filled'
+                  style={textfieldstyle}
+                  name='endDate'
+                  type='date'
+                  onChange={formik.handleChange}
+                  value={formik.values.endDate}
+                  error={formik.touched.endDate && Boolean(formik.errors.endDate)}
+                  helperText={formik.touched.endDate && formik.errors.endDate}
+                  placeholder='Registration end date'
+                  fullWidth
+                />
+              </Grid>
             </Grid>
-            <Grid item xs={12} md={6}>
-              <TextField
-                varient='filled'
-                style={textfieldstyle}
-                name='endDate'
-                type='date'
-                onChange={formik.handleChange}
-                value={formik.values.endDate}
-                error={formik.touched.endDate && Boolean(formik.errors.endDate)}
-                helperText={formik.touched.endDate && formik.errors.endDate}
-                placeholder='Registration end date'
-                fullWidth
-              />
+          </CardContent>
+        </Card>
+
+        <Typography variant='h6' color="white" marginTop={10} marginBottom={2}>
+          Edit details of your car:
+        </Typography>
+        <Card>
+          <CardContent>
+            <Grid container spacing={2}>
+              <Grid item xs={12} md={6}>
+                <TextField
+                  label="Car Model"
+                  varient='filled'
+                  style={textfieldstyle}
+                  name='model'
+                  onChange={formik.handleChange}
+                  value={formik.values.model}
+                  error={formik.touched.model && Boolean(formik.errors.model)}
+                  helperText={formik.touched.model && formik.errors.model}
+                  placeholder='Car model'
+                  fullWidth
+                />
+              </Grid>
+              <Grid item xs={12} md={6}>
+                <TextField
+                  label="Car make"
+                  varient='filled'
+                  style={textfieldstyle}
+                  name='make'
+                  onChange={formik.handleChange}
+                  value={formik.values.make}
+                  error={formik.touched.make && Boolean(formik.errors.make)}
+                  helperText={formik.touched.make && formik.errors.make}
+                  placeholder='Car make'
+                  fullWidth
+                />
+              </Grid>
             </Grid>
+            <Grid container spacing={2}>
+              <Grid item xs={12} md={6}>
+                <TextField
+                  label="Car Type"
+                  varient='filled'
+                  style={textfieldstyle}
+                  name='type'
+                  onChange={formik.handleChange}
+                  value={formik.values.type}
+                  error={formik.touched.type && Boolean(formik.errors.type)}
+                  helperText={formik.touched.type && formik.errors.type}
+                  placeholder='Car Type'
+                  fullWidth
+                />
+              </Grid>
+              <Grid item xs={12} md={6}>
+                <TextField
+                  label="Gear type"
+                  varient='filled'
+                  style={textfieldstyle}
+                  name='gear'
+                  onChange={formik.handleChange}
+                  value={formik.values.gear}
+                  error={formik.touched.gear && Boolean(formik.errors.gear)}
+                  helperText={formik.touched.gear && formik.errors.gear}
+                  placeholder='Gear type'
+                  fullWidth
+                />
+              </Grid>
+            </Grid>
+            <Grid container spacing={2}>
+              <Grid item xs={12} md={6}>
+                <TextField
+                  label="No. of Seats"
+                  varient='filled'
+                  style={textfieldstyle}
+                  name='seats'
+                  onChange={formik.handleChange}
+                  value={formik.values.seats}
+                  error={formik.touched.seats && Boolean(formik.errors.seats)}
+                  helperText={formik.touched.seats && formik.errors.seats}
+                  placeholder='No. of Seats'
+                  fullWidth
+                />
+              </Grid>
+              <Grid item xs={12} md={6}>
+                <TextField
+                  label="Price per day"
+                  varient='filled'
+                  style={textfieldstyle}
+                  name='price'
+                  type='number'
+                  onChange={formik.handleChange}
+                  value={formik.values.price}
+                  error={formik.touched.price && Boolean(formik.errors.price)}
+                  helperText={formik.touched.price && formik.errors.price}
+                  placeholder='Price per day'
+                  fullWidth
+                />
+              </Grid>
+            </Grid>
+          </CardContent>
+        </Card>
+
+        {/* upload image of car */}
+        <Typography variant='h6' color="white" marginTop={10} marginBottom={2}>
+          Please upload an image of your car:
+        </Typography>
+        <Grid container spacing={2}>
+          <Grid item xs={12} md={6}>
+            <input
+              accept="image/*"
+              style={{ display: 'none' }}
+              id="raised-button-file"
+              multiple
+              type="file"
+            />
+            <label htmlFor="raised-button-file">
+              <Button variant="raised" component="span" style={{ backgroundColor: '#FF4E00', color: 'white' }}>
+                Upload Image
+              </Button>
+            </label>
           </Grid>
-        </CardContent>
-      </Card>
-
-      <Typography variant='h6' color="white" marginTop={10} marginBottom={2}>
-        Edit details of your car:
-      </Typography>
-      <Card>
-        <CardContent>
-          <Grid container spacing={2}>
-        <Grid item xs={12} md={6}>
-          <TextField
-            label="Car Model"
-            varient='filled'
-            style={textfieldstyle}
-            name='model'
-            onChange={formik.handleChange}
-            value={formik.values.model}
-            error={formik.touched.model && Boolean(formik.errors.model)}
-            helperText={formik.touched.model && formik.errors.model}
-            placeholder='Car model'
-            fullWidth
-          />
         </Grid>
-        <Grid item xs={12} md={6}>
-          <TextField
-            label="Car make"
-            varient='filled'
-            style={textfieldstyle}
-            name='make'
-            onChange={formik.handleChange}
-            value={formik.values.make}
-            error={formik.touched.make && Boolean(formik.errors.make)}
-            helperText={formik.touched.make && formik.errors.make}
-            placeholder='Car make'
-            fullWidth
-          />
-        </Grid>
-      </Grid>
-      <Grid container spacing={2}>
-        <Grid item xs={12} md={6}>
-          <TextField
-            label="Car Type"
-            varient='filled'
-            style={textfieldstyle}
-            name='type'
-            onChange={formik.handleChange}
-            value={formik.values.type}
-            error={formik.touched.type && Boolean(formik.errors.type)}
-            helperText={formik.touched.type && formik.errors.type}
-            placeholder='Car Type'
-            fullWidth
-          />
-        </Grid>
-        <Grid item xs={12} md={6}>
-          <TextField
-            label="Gear type"
-            varient='filled'
-            style={textfieldstyle}
-            name='gear'
-            onChange={formik.handleChange}
-            value={formik.values.gear}
-            error={formik.touched.gear && Boolean(formik.errors.gear)}
-            helperText={formik.touched.gear && formik.errors.gear}
-            placeholder='Gear type'
-            fullWidth
-          />
-        </Grid>
-      </Grid>
-      <Grid container spacing={2}>
-        <Grid item xs={12} md={6}>
-          <TextField
-            label="No. of Seats"
-            varient='filled'
-            style={textfieldstyle}
-            name='seats'
-            onChange={formik.handleChange}
-            value={formik.values.seats}
-            error={formik.touched.seats && Boolean(formik.errors.seats)}
-            helperText={formik.touched.seats && formik.errors.seats}
-            placeholder='No. of Seats'
-            fullWidth
-          />
-        </Grid>
-        <Grid item xs={12} md={6}>
-          <TextField
-           label="Price per day"
-            varient='filled'
-            style={textfieldstyle}
-            name='price'
-            type='number'
-            onChange={formik.handleChange}
-            value={formik.values.price}
-            error={formik.touched.price && Boolean(formik.errors.price)}
-            helperText={formik.touched.price && formik.errors.price}
-            placeholder='Price per day'
-            fullWidth
-          />
-        </Grid>
-      </Grid>
-        </CardContent>
-      </Card>
-      
-      {/* upload image of car */}
-      <Typography variant='h6' color="white" marginTop={10} marginBottom={2}>
-        Please upload an image of your car:
-      </Typography>
-      <Grid container spacing={2}>
-        <Grid item xs={12} md={6}>
-          <input
-            accept="image/*"
-            style={{ display: 'none' }}
-            id="raised-button-file"
-            multiple
-            type="file"
-          />
-          <label htmlFor="raised-button-file">
-            <Button variant="raised" component="span" style={{ backgroundColor: '#FF4E00', color: 'white' }}>
-              Upload Image
-            </Button>
-          </label>
-        </Grid>
-      </Grid>
-      <br />
-      <Button type='submit' variant="contained" color='btn' style={btnstyle} >
-        Save Details
-      </Button>
-    </Box>
-  </Container>
+        <br />
+        <Button type='submit' variant="contained" color='btn' style={btnstyle} >
+          Save Details
+        </Button>
+      </Box>
+    </Container>
   )
 }
 
