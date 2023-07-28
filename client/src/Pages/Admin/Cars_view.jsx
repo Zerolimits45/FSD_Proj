3daf0da8b7ae57bad2c238dd891afa2bb68291e4
TanBenneT <tanbennet02@gmail.com>
2023-07-28 20:45:14 +0800

    Feedback done, edited car dropdown values

--- client/src/Pages/Admin/Cars_view.jsx
+++ client/src/Pages/Admin/Cars_view.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react'
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
+import { Dialog, DialogTitle, DialogContent, DialogContentText, DialogActions } from '@mui/material';
 import { Button } from '@mui/material'
 import { DataGrid } from '@mui/x-data-grid'
 import http from '../../http'
@@ -7,6 +8,14 @@ import http from '../../http'
 
 function RenderButton(props) {
     const { carId } = props;
+    const navigate = useNavigate();
+    const [open, setOpen] = useState(false);
+    const handleOpen = () => {
+        setOpen(true);
+    };
+    const handleClose = () => {
+        setOpen(false);
+    };
 
     return (
         <>
@@ -22,9 +31,36 @@ function RenderButton(props) {
                 variant="contained"
                 size="small"
                 style={{ marginLeft: 16, backgroundColor: '#C70000' }}
+                onClick={handleOpen}
             >
                 Delete
             </Button>
+
+            <Dialog open={open} onClose={handleClose}>
+                <DialogTitle>
+                    Delete Car
+                </DialogTitle>
+                <DialogContent>
+                    <DialogContentText>
+                        Are you sure you want to delete this Car?
+                    </DialogContentText>
+                </DialogContent>
+                <DialogActions>
+                    <Button variant="contained" color="inherit"
+                        onClick={handleClose}>
+                        Cancel
+                    </Button>
+                    <Button variant="contained" color="error"
+                        onClick={() => {
+                            http.delete(`/car/${carId}`).then((res) => {
+                                console.log(res.data)
+                                navigate('/admin/dashboard')
+                            });
+                        }}>
+                        Delete
+                    </Button>
+                </DialogActions>
+            </Dialog>
         </>
     );
 }
@@ -39,7 +75,7 @@ const columns = [
     { field: 'gear', headerName: 'Gear', width: 100 },
     { field: 'seats', headerName: 'Seats', width: 70 },
     { field: 'price', headerName: 'Price', width: 70 },
-    { field: 'action', headerName: 'Actions', width: 200, renderCell:(params) => <RenderButton carId={params.row.id} />  },
+    { field: 'action', headerName: 'Actions', width: 200, renderCell: (params) => <RenderButton carId={params.row.id} /> },
 ];
 
 function Cars_view() {
